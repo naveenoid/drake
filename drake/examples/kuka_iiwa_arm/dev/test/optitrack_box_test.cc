e3961f6e7a11b16860b8fcb7f21d4b6b6b2b0aff
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2017-05-01 14:12:33 -0400

    dummy test added

---
+++ drake/examples/kuka_iiwa_arm/dev/test/optitrack_box_test.cc
@@ -0,0 +1,24 @@
+#include "optitrack/optitrack_frame_t.hpp"
+
+namespace drake {
+namespace examples {
+namespace kuka_iiwa_arm {
+namespace {
+int do_main() {
+
+
+
+  return 0;
+}
+
+} // namespace
+} // namespace kuka_iiwa_arm
+} // namesapce examples
+} // namespace drake
+
+int main(int argc, char *argv[]) {
+  return drake::examples::kuka_iiwa_arm::do_main();
+}
+
+
+
