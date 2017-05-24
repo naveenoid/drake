c61be070e6418c21c024f3f1fce0b46479fc0e2e
Naveen Kuppuswamy <naveen.kuppuswamy@tri.global>
2017-05-24 22:08:26 +0200

    WIP : almost there

--- drake/examples/kuka_iiwa_arm/dev/whole_body_contact/demo_tree_visual_test.cc
+++ drake/examples/kuka_iiwa_arm/dev/whole_body_contact/demo_tree_visual_test.cc
@@ -14,9 +14,9 @@
 #include "drake/multibody/rigid_body_tree.h"
 #include "drake/examples/kuka_iiwa_arm/dev/whole_body_contact/demo_diagram_builder.h"
 #include "drake/examples/kuka_iiwa_arm/dev/tools/simple_tree_visualizer.h"
-
-DEFINE_int32(num_configurations, 10,
-"Number of random test configurations to display in the demo");
+//
+//DEFINE_int32(num_configurations, 10,
+//"Number of random test configurations to display in the demo");
 
 namespace drake {
 namespace examples {
@@ -40,21 +40,38 @@ int DoMain() {
 
   SimpleTreeVisualizer simple_tree_visualizer(*tree.get(), &lcm);
 
-  // Simple demo that iterates through a bunch of joint configurations.
-  for (int i = 0; i < FLAGS_num_configurations; ++i) {
 
    Eigen::VectorXd robot_config = Eigen::VectorXd::Zero(7);
-   Eigen::VectorXd box_config = Eigen::VectorXd::Random(4);
 
-    std::cout<<"Tree positions : "<<tree->get_num_positions()<<", "<<
-               tree->get_num_positions()<<"\n"
-    simple_tree_visualizer.visualize(Eigen::VectorXd::Random(
-        tree->get_num_positions()));
+  robot_config << 0.75 , 1.5, 1.5, 1.75, 0.0, -1.5, 0.5;
+
+   Eigen::VectorXd box_config = Eigen::VectorXd::Zero(7);
+   box_config.head(3) = (Eigen::Vector3d() << -0.30, 0.5, 0.15).finished();
+
+  Eigen::AngleAxisd aa(0.0, Eigen::Vector3d::UnitZ());
+  Eigen::Quaterniond quat(aa);
+  quat.normalize();
+   box_config.tail(4) = quat.vec();
+
+
+   std::cout<<"Tree positions : "<<tree->get_num_positions()<<", "<<
+               tree->get_num_positions()<<"\n";
+
+    std::cout<<"Box position : "<<box_config<<"\n";
+   std::cout<<"Box orientation : "<<quat.vec()<<"\n";
+   Eigen::VectorXd state = Eigen::VectorXd::Zero(
+        tree->get_num_positions());
+    state.head(7) = robot_config;
+    state.tail(7) = box_config;
+   // state.tail(3) = box_position;
+    simple_tree_visualizer.visualize(state);
+
+    std::cout<<"State : "<<state<<"\n";
 
     // Sleep for a second just so that the new configuration can be seen
     // on the visualizer.
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
+    std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+
 
   return 0;
 }
--- drake/examples/kuka_iiwa_arm/dev/whole_body_contact/whole_body_common.h
+++ drake/examples/kuka_iiwa_arm/dev/whole_body_contact/whole_body_common.h
@@ -4,6 +4,28 @@ namespace drake {
 namespace examples {
 namespace kuka_iiwa_arm {
 namespace whole_body_contact {
+//
+//// These constants are used in several locations in the Monolithic
+//// pick and place demo.
+//const char kIiwaUrdf[] =
+//    "/manipulation/models/iiwa_description/urdf/"
+//        "iiwa14_polytope_collision.urdf";
+//const char kIiwaEndEffectorName[] = "iiwa_link_ee";
+
+
+// Different states for the pick and place task.
+enum PickAndPlaceState {
+  APPROACH_PICK_PREGRASP,
+  APPROACH_PICK,
+  GRASP,
+  LIFT_FROM_PICK,
+  APPROACH_PLACE_PREGRASP,
+  APPROACH_PLACE,
+  PLACE,
+  LIFT_FROM_PLACE,
+  DONE,
+};
+
 
 } // namespace whole_body_contact
 } // namespace kuka_iiwa_arm
